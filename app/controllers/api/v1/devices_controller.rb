da61e275ddf815953e543a8dc2f7576bcdcaba1f
wukerplank <wukerplank@edthofer.at>
2014-03-24 16:19:20 +0100

    display live devices and add configured devices

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,16 +1,31 @@
 class Api::V1::DevicesController < ApplicationController
   
   def index
-    @device_array = [
-      {id: 1, uuid: SecureRandom.uuid},
-      {id: 2, uuid: SecureRandom.uuid},
-      {id: 3, uuid: SecureRandom.uuid},
-      {id: 4, uuid: SecureRandom.uuid}
-    ]
+    @device_array = HiveRpcWrapper::Device.configured
     
     respond_to do |format|
       format.json { render json: @device_array }
     end
+  end
+  
+  def supported
+    @device_array = HiveRpcWrapper::Device.supported
+    
+    respond_to do |format|
+      format.json { render json: @device_array }
+    end
+  end
+  
+  def create
+    device_class_id = params[:device].delete(:deviceClassId)
+    
+    HiveRpcWrapper::Device.add(device_class_id, device_params)
+  end
+  
+  private
+  
+  def device_params
+    params.require(:device).permit!
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ HiveRails::Application.routes.draw do
   namespace :api do
     namespace :v1 do
       
-      resources :devices
+      resources :devices do
+        collection do
+          get 'supported' => 'devices#supported'
+        end
+      end
       resources :rules
       
     end
